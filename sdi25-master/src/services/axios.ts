4c30f13377c42f06a28d1250b278fc64ffe6be1c
SYNDOUKONE <syndoukoney@gmail.com>
2026-02-15 18:06:58 +0000

    Implement request interceptor for axios
    Add request interceptor to include authorization token

--- sdi25-master/src/services/axios.ts
+++ sdi25-master/src/services/axios.ts
@@ -1,11 +1,25 @@
 import axios from "axios";
+import secureLocalStorage from "react-secure-storage";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
+    Accept: "application/json",
     "Content-Type": "application/json",
   },
 });
 
-export default api;
+api.interceptors.request.use(
+  (config) => {
+    const token = secureLocalStorage.getItem("session_token");
 
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
